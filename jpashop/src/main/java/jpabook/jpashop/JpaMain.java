37a954910cdb4195ba3c7871087c47e59799f088
yeseul106 <20191037@sungshin.ac.kr>
2023-02-09 16:05:32 +0900

    [ADD] section4 - 실전 예제 프로젝트

---
+++ jpashop/src/main/java/jpabook/jpashop/JpaMain.java
@@ -0,0 +1,29 @@
+package jpabook.jpashop;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");//EntityManagerFactory -> 애플리케이션 로딩 시점에 딱 한번만 선언
+
+        EntityManager em = emf.createEntityManager(); //트랜잭션 단위로 생성 해줘야함.
+
+        EntityTransaction tx = em.getTransaction(); //jpa는 트랜잭션 단위 안에서 DB 접근해야함.
+        tx.begin();
+
+        //* 회원 생성
+        try {
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+}
+
